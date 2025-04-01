42e36b9d39ed1c342b0bd1efec8fb8de976874e1
Vasundhara2894 <vasundhara2894@gmail.com>
2025-04-01 22:17:43 +0530

    Update Assign-Cookies.cpp

--- Assign-Cookies.cpp
+++ Assign-Cookies.cpp
@@ -23,3 +23,12 @@ public:
         
     }
 };
+// Greedy algorithm -Easy level problem
+
+// WHAT IS THE PROBLEM?
+
+// There are two arrays- one with greed factor and other with cookies The children have the greed factor to satisfy them we need to provide them the cookies whoose size is greater than the greed factor.
+// We return the maximum number of children who are satisfied.
+// SOLUTION Sort the two array so that smallest cookie gets assigned to least greedy child. 
+// Take two pointer and start comapairing until condition fails.
+// TIME COMPLEXITY- O(N log N+M log M+ M+N) SORT AND WHILE LOOP SPACE COMPLEXITY-O(1) CONSTANT SPACE
